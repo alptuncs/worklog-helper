b3f3361663c9010056544ddf1ede5db76a18de0b
alptuncs <alptuncsever@gmail.com>
2022-11-01 09:53:13 +0300

    Add Customer option to add item

--- AutoWorklog/AddLogCommand.cs
+++ AutoWorklog/AddLogCommand.cs
@@ -2,6 +2,7 @@
 using Gazel.Client.CommandLine;
 using Gazel.Logging;
 using Newtonsoft.Json;
+using System;
 
 namespace AutoWorklog;
 
@@ -21,9 +22,62 @@ public class AddLogCommand : CommandBase<AddLogOptions>
 
     public override async Task Execute()
     {
+        if (Args.Customer == null) throw new Exception("Customer can not be null");
+
         if (Args.Path != null) Path = Args.Path;
 
-        string text = await File.ReadAllTextAsync(Path + "test.workreport.json");
+        if (!File.Exists(Path + Args.Customer + ".workreport.json"))
+        {
+            log.Info("Customer workreport file could not be found, create one ?");
+
+            var options = new List<string>();
+            options.Add("y");
+            options.Add("n");
+
+            foreach (var option in options)
+            {
+                Console.WriteLine(option);
+            }
+
+            var originalpos = Console.CursorTop();
+            var currentpos = originalpos;
+            var k = Console.ReadKey();
+
+            while (k.Key != ConsoleKey.Enter)
+            {
+                if (k.Key == ConsoleKey.UpArrow)
+                {
+                    currentpos--;
+                }
+
+                if (k.Key == ConsoleKey.DownArrow)
+                {
+                    currentpos++;
+                }
+
+                currentpos = Math.Min(originalpos - 1, Math.Max(originalpos - options.Count, currentpos));
+
+                MoveCursor(originalpos, currentpos, options);
+
+                Console.SetCursorPosition(0, originalpos);
+                k = Console.ReadKey();
+            }
+
+            if (currentpos == originalpos - 2)
+            {
+                File.WriteAllText(Path + Args.Customer + ".workreport.json", "{}");
+            }
+            else
+            {
+                log.Info("Without a file you can't add log entries...");
+
+                return;
+            }
+        }
+
+        string text = await File.ReadAllTextAsync(Path + Args.Customer + ".workreport.json");
+
+        dynamic MyDynamic = new System.Dynamic.ExpandoObject();
 
         var json = JsonConvert.DeserializeObject<WorkLogTask>(text);
 
@@ -33,13 +87,10 @@ public class AddLogCommand : CommandBase<AddLogOptions>
 
         if (Args.Start != null)
         {
-            json.log.FirstOrDefault().logEntries.Add(new(Args.Start, Args.End, Args.Task));
+            logEntryList.Add(new(Args.Start, Args.End, Args.Task));
         }
         else
         {
-
-
-
             while (true)
             {
                 var input = Console.ReadLine();
@@ -49,7 +100,7 @@ public class AddLogCommand : CommandBase<AddLogOptions>
                 if (input != null)
                 {
                     var logEntry = new LogEntry(input.Substring(0, 5), input.Substring(6, 5), input.Substring(11));
-                    json.log.FirstOrDefault().logEntries.Add(logEntry);
+                    logEntryList.Add(logEntry);
                 }
             }
         }
@@ -60,14 +111,40 @@ public class AddLogCommand : CommandBase<AddLogOptions>
         var prList = new List<string>();
         prList.Add("Github");
         WorkLogTask serializeTest = new WorkLogTask(prList, dailyLogList);
+        MyDynamic.Worklog = serializeTest;
 
 
 
-        string jsonOut = JsonConvert.SerializeObject(serializeTest, Formatting.Indented);
+        string jsonOut = JsonConvert.SerializeObject(MyDynamic, Formatting.Indented);
 
         File.WriteAllText(Path + "test.workreport.json", jsonOut);
 
-        Console.WriteLine(json);
+        Console.WriteLine(jsonOut);
+    }
+    public void SetBackgroundColor()
+    {
+        Console.SetForeGroundColor(ConsoleColor.Black);
+        Console.SetBackgroundColor(ConsoleColor.White);
+    }
+
+    public void MoveCursor(int originalpos, int currentpos, List<string> options)
+    {
+        Console.SetCursorPosition(0, currentpos);
+
+        HighlightCurrentPos(originalpos, currentpos, options);
+    }
+
+    public void HighlightCurrentPos(int originalpos, int currentpos, List<string> options)
+    {
+        SetBackgroundColor();
+        Console.WriteLine(options[options.Count - (originalpos - currentpos)]);
+
+        Console.ResetColor();
+
+        Console.SetCursorPosition(0, (int)(currentpos + Math.Pow(-1, originalpos - currentpos)));
+        Console.WriteLine(options[(originalpos - currentpos + 1) % 2]);
+
+        Console.SetCursorPosition(0, currentpos);
     }
 }
 
@@ -82,12 +159,15 @@ public class AddLogOptions : IOptions
     public string Task { get; }
     [Option('p', "path")]
     public string Path { get; }
+    [Option('c', "customer")]
+    public string Customer { get; }
 
-    public AddLogOptions(string start, string end, string task, string path)
+    public AddLogOptions(string start, string end, string task, string path, string customer)
     {
         Start = start;
         End = end;
         Task = task;
         Path = path;
+        Customer = customer;
     }
 }
--- AutoWorklog/DailyLog.cs
+++ AutoWorklog/DailyLog.cs
@@ -5,10 +5,10 @@ namespace AutoWorklog;
 
 public class DailyLog
 {
-    //[JsonProperty(PropertyName = "03")]
-    public List<LogEntry> logEntries { get; set; }
+    public dynamic LogEntries { get; set; }
     public DailyLog(List<LogEntry> logEntries)
     {
-        this.logEntries = logEntries;
+        LogEntries = new System.Dynamic.ExpandoObject();
+        LogEntries.logs = (logEntries);
     }
 }
